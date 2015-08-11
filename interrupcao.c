856a780ddbdd246ceabd7f5271319c937e3334c5
Marco Antonio <marco@ifc-sombrio.edu.br>
2015-08-11 18:01:57 -0300

    Alteração no fonte em C, para o exercicio

--- interrupcao.c
+++ interrupcao.c
@@ -4,7 +4,7 @@
 */
 
 # include <stdio.h>
-/////////////////////////////////////////
+
 void rotinaDoDisco()  // Definição da função "rotinaDoDisco"
 {
     printf("Atendendo solicitação do Disco...\n");
@@ -12,30 +12,12 @@ void rotinaDoDisco()  // Definição da função "rotinaDoDisco"
 
 }
 
-void rotinaDaRede()  // Definição da função "rotinaDaRede"
-{
-    printf("Atendendo solicitação da Rede...\n");
-    sleep(2); //espera dois segundos
-
-}
-
-void rotinaDoMouse()  // Definição da função "rotinaDoMouse"
-{
-    printf("Atendendo solicitação do Mouse...\n");
-    sleep(2); //espera dois segundos
-
-}
-
 int main() {
     char interrupcao;
     char loop='t'; // loop recebe 't' (true)
-    //cria o vetor de interrupções
-    int vi[3] = {1, 2, 3};
 
     printf("Selecione \n \
     d - para uma interupção de disco \n \
-    r - para uma interrupçãoda placa de Rede \n \
-    m - para uma nterrupção do mouse \n \
     s - para sair ");
 
 
@@ -47,14 +29,6 @@ while(loop != 'f'){ // enquanto a variavel loop for diferente de 'f' , faça
     switch(interrupcao) {
         case 'd':
             rotinaDoDisco(); // chama a função de tratamento do disco
-            printf("pronto\n");
-            break;
-        case 'r':
-            rotinaDaRede();
-            printf("pronto\n");
-            break;
-        case 'm':
-            rotinaDoMouse();
             printf("pronto\n");
             break;
         case 's':
